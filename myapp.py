1c47f59223761c94623a890b4e8f17840b9db1a8
netcool <51483807+toksichny@users.noreply.github.com>
2022-07-24 13:20:46 +0300

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -6,10 +6,6 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
-@app.route('/payeer_1706208548.txt')
-def payeer_1706208548():
-    return render_template('payeer_1706208548.txt')
-
 
 if __name__ == '__main__':
     app.run(debug=True)
